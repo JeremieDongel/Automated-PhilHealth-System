dbeba2b99303c34dce2de432b53c14e0422b45c9
JeremieDongel <123613203+JeremieDongel@users.noreply.github.com>
2025-04-14 11:53:12 +0800

    Create models.py

---
+++ models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+class Medicine(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
